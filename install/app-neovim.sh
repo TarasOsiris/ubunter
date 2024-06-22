d2fc37982922ed75e8e200aa8ed6082e3075a03a
Taras Leskiv <leskiv.taras@gmail.com>
2024-06-22 20:51:30 +0300

    wip

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -1,7 +1,12 @@
+sudo apt install -y ninja-build gettext cmake unzip curl build-essential
+
 cd /tmp
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
+
+# cleanup
+cd /tmp
 rm -rf neovim
 cd -
 
