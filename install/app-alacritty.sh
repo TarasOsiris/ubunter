0b6e130c2de638bea977a47801dc4d02f0fabe7e
Taras Leskiv <leskiv.taras@gmail.com>
2024-06-22 22:31:22 +0300

    wip

--- install/app-alacritty.sh
+++ install/app-alacritty.sh
@@ -1,3 +1,5 @@
 sudo apt install -y cargo cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 cargo install alacritty
 
+mkdir -p ~/.config/alacritty
+ln -s ~/.local/share/ubunter/config/alacritty.toml ~/.config/alacritty/alacritty.toml
