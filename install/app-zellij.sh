f9dfe1212c28990bcbe379bff9f4d4cfeffc4b76
Taras Leskiv <leskiv.taras@gmail.com>
2024-06-22 19:11:47 +0300

    wip

--- install/app-zellij.sh
+++ install/app-zellij.sh
@@ -5,4 +5,8 @@ sudo install zellij /usr/local/bin
 rm zellij.tar.gz zellij
 cd -
 
+mkdir -p ~/.config/zellij/
+[ ! -L "$HOME/.config/zellij/themes" ] && ln -s ~/.local/share/ubunter/themes/zellij ~/.config/zellij/themes
+
+
 # see https://github.com/basecamp/omakub/blob/master/install/app-zellij.sh
