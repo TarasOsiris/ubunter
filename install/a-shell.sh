9ba03d749e78e0c60836b676297a5def1a23bc62
Taras Leskiv <leskiv.taras@gmail.com>
2024-06-23 13:13:31 +0000

    --wip-- [skip ci]

--- install/a-shell.sh
+++ install/a-shell.sh
@@ -1,9 +1,6 @@
 sudo apt install -y zsh
 
-# if [ ! -d "$HOME/.oh-my-zsh" ]; then
-# 	rm -rf "$HOME/.oh-my-zsh"
-# fi
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 mkdir -p ~/.fzf
 
