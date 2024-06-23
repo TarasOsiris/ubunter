2f7e3a530a63f119e80a9bf11460f9cb25dd4b22
Taras Leskiv <leskiv.taras@gmail.com>
2024-06-23 13:17:59 +0000

    --wip-- [skip ci]

---
+++ install/opt/app-jetbrains-toolbox.sh
@@ -0,0 +1,5 @@
+cd /tmp
+
+sudo apt install -y libfuse2 libxi6 libxrender1 libxtst6 mesa-utils libfontconfig libgtk-3-bin tar
+
+curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
