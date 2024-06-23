cfd7c8f0f59a845f6e9ed255ddc07f7040d20074
Taras Leskiv <leskiv.taras@gmail.com>
2024-06-23 22:28:30 +0300

    wip

--- install/app-alacritty.sh
+++ install/app-alacritty.sh
@@ -1,6 +1,6 @@
 sudo apt install -y alacritty
 
-mdkir -p ~/.config/alacritty
+mkdir -p ~/.config/alacritty
 
 rm -f ~/.config/alacritty/alacritty.toml
 ln -s ~/.local/share/ubunter/config/alacritty.toml ~/.config/alacritty/alacritty.toml
